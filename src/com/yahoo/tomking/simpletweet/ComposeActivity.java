0159c984aed3095299392adea106623bb114709e
Tom King <mobilemind@pobox.com>
2014-12-07 21:59:35 -0800

    add toast explaining use of Back (hardware) button

--- src/com/yahoo/tomking/simpletweet/ComposeActivity.java
+++ src/com/yahoo/tomking/simpletweet/ComposeActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.yahoo.tomking.simpletweet.R;
@@ -17,18 +18,18 @@ public class ComposeActivity extends Activity {
 	private Tweet tweet;
 	public static final int REQUEST_CODE = 0;
 
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Toast.makeText(this, "Use Back to cancel", Toast.LENGTH_SHORT).show();
 		setContentView(R.layout.activity_compose);
 		client = TwitterApplication.getRestClient();
 
 	}
-	
+
 	public void onSubmit(View view) {
 		EditText etComposeTweet = (EditText) findViewById(R.id.etComposeTweet);
-		
+
 		String tweetText = etComposeTweet.getText().toString();
 		client.postNewTweet(new JsonHttpResponseHandler() {
 			@Override
--- src/com/yahoo/tomking/simpletweet/ProfileActivity.java
+++ src/com/yahoo/tomking/simpletweet/ProfileActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.nostra13.universalimageloader.core.ImageLoader;
@@ -21,9 +22,10 @@ public class ProfileActivity extends FragmentActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Toast.makeText(this, "Use Back to return to timeline", Toast.LENGTH_SHORT).show();
 		setContentView(R.layout.activity_profile);
 		Intent i = getIntent();
-		User user = (User)i.getSerializableExtra("user");
+		User user = (User) i.getSerializableExtra("user");
 		userTimelineFragment = (UserTimelineFragment) getFragmentManager().findFragmentById(R.id.fragmentUserTimeline);
 		userTimelineFragment.setUser(user);
 		loadProfileInfo(user);
@@ -31,22 +33,22 @@ public class ProfileActivity extends FragmentActivity {
 
 	private void loadProfileInfo(User user) {
 		if (user == null) {
-			TwitterApplication.getRestClient().getMyInfo(new JsonHttpResponseHandler() {
-				@Override
-				public void onSuccess(JSONObject json) {
-					User u = User.fromJson(json);
-					loadProfileResults(u);
-				}
+			TwitterApplication.getRestClient().getMyInfo(
+					new JsonHttpResponseHandler() {
+						@Override
+						public void onSuccess(JSONObject json) {
+							User u = User.fromJson(json);
+							loadProfileResults(u);
+						}
 
-				
-			});
+					});
 		} else {
 			loadProfileResults(user);
 		}
 	}
-	
+
 	private void loadProfileResults(User u) {
-		getActionBar().setTitle( "@" + u.getScreenName());
+		getActionBar().setTitle("@" + u.getScreenName());
 		populateProfileHeader(u);
 		populateProfileList(u);
 	}
@@ -58,16 +60,17 @@ public class ProfileActivity extends FragmentActivity {
 	private void populateProfileHeader(User u) {
 		TextView tvName = (TextView) findViewById(R.id.tvName);
 		TextView tvBio = (TextView) findViewById(R.id.tvBio);
-		TextView tvFollowers = (TextView)findViewById(R.id.tvFollowers);
-		TextView tvFollowing = (TextView)findViewById(R.id.tvFollowing);
-		ImageView ivProfileImage = (ImageView)findViewById(R.id.ivProfileImage);
-		
+		TextView tvFollowers = (TextView) findViewById(R.id.tvFollowers);
+		TextView tvFollowing = (TextView) findViewById(R.id.tvFollowing);
+		ImageView ivProfileImage = (ImageView) findViewById(R.id.ivProfileImage);
+
 		tvName.setText(u.getName());
 		tvBio.setText(u.getDescription());
 		tvFollowers.setText(u.getFollowersCount() + " Followers");
 		tvFollowing.setText(u.getFriendsCount() + " Following");
-		
-		ImageLoader.getInstance().displayImage(u.getProfileImageUrl(), ivProfileImage);
-		
+
+		ImageLoader.getInstance().displayImage(u.getProfileImageUrl(),
+				ivProfileImage);
+
 	}
 }
