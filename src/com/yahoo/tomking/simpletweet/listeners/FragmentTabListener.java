55e3231d8b0281be7bc4eba4dc3fa76b51bfecdb
Tom King <mobilemind@pobox.com>
2014-12-07 22:03:43 -0800

    update Tab listener; try to make re-Select = Refresh

--- src/com/yahoo/tomking/simpletweet/listeners/FragmentTabListener.java
+++ src/com/yahoo/tomking/simpletweet/listeners/FragmentTabListener.java
@@ -6,68 +6,73 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
- 
+
 public class FragmentTabListener<T extends Fragment> implements TabListener {
-    private Fragment mFragment;
-    private final Activity mActivity;
-    private final String mTag;
-    private final Class<T> mClass;
-    private final int mfragmentContainerId;
-    private final Bundle mfragmentArgs;
+	private Fragment mFragment;
+	private final Activity mActivity;
+	private final String mTag;
+	private final Class<T> mClass;
+	private final int mfragmentContainerId;
+	private final Bundle mfragmentArgs;
 
+	public FragmentTabListener(Activity activity, String tag, Class<T> clz) {
+		mActivity = activity;
+		mTag = tag;
+		mClass = clz;
+		mfragmentContainerId = android.R.id.content;
+		mfragmentArgs = new Bundle();
+	}
 
-    public FragmentTabListener(Activity activity, String tag, Class<T> clz) {
-        mActivity = activity;
-        mTag = tag;
-        mClass = clz;
-        mfragmentContainerId = android.R.id.content;
-        mfragmentArgs = new Bundle();
-    }
-        
-    public FragmentTabListener(int fragmentContainerId, Activity activity, 
-            String tag, Class<T> clz) {
-        mActivity = activity;
-        mTag = tag;
-        mClass = clz;
-        mfragmentContainerId = fragmentContainerId;
-        mfragmentArgs = new Bundle();
-    }
-    
-    public FragmentTabListener(int fragmentContainerId, Activity activity, 
-            String tag, Class<T> clz, Bundle args) {
-        mActivity = activity;
-        mTag = tag;
-        mClass = clz;
-        mfragmentContainerId = fragmentContainerId;
-        mfragmentArgs = args;
-    }
- 
- 
-    @Override
+	public FragmentTabListener(int fragmentContainerId, Activity activity, String tag, Class<T> clz) {
+		mActivity = activity;
+		mTag = tag;
+		mClass = clz;
+		mfragmentContainerId = fragmentContainerId;
+		mfragmentArgs = new Bundle();
+	}
+
+	public FragmentTabListener(int fragmentContainerId, Activity activity, String tag, Class<T> clz, Bundle args) {
+		mActivity = activity;
+		mTag = tag;
+		mClass = clz;
+		mfragmentContainerId = fragmentContainerId;
+		mfragmentArgs = args;
+	}
+
+	@Override
 	public void onTabSelected(Tab tab, android.app.FragmentTransaction ft) {
-        FragmentTransaction sft = mActivity.getFragmentManager().beginTransaction();
-        if (mFragment == null) {
-            // instantiate & initialize fragment for this activity
-            mFragment = Fragment.instantiate(mActivity, mClass.getName(), mfragmentArgs);
-            sft.add(mfragmentContainerId, mFragment, mTag);
-        } else {
-            sft.attach(mFragment);
-        }
-        sft.commit();
-    }
- 
-    @Override
+		FragmentTransaction sft = mActivity.getFragmentManager().beginTransaction();
+		if (mFragment == null) {
+			// instantiate & initialize fragment for this activity
+			mFragment = Fragment.instantiate(mActivity, mClass.getName(), mfragmentArgs);
+			sft.add(mfragmentContainerId, mFragment, mTag);
+		} else {
+			sft.attach(mFragment);
+		}
+		sft.commit();
+	}
+
+	@Override
 	public void onTabUnselected(Tab tab, android.app.FragmentTransaction ft) {
-        FragmentTransaction sft = mActivity.getFragmentManager().beginTransaction();
-        if (mFragment != null) {
-            // detach old fragment to prepare for new
-            sft.detach(mFragment);
-        }
-        sft.commit();
-    }
- 
-    @Override
+		FragmentTransaction sft = mActivity.getFragmentManager().beginTransaction();
+		if (mFragment != null) {
+			// detach old fragment to prepare for new
+			sft.detach(mFragment);
+		}
+		sft.commit();
+	}
+
+	@Override
 	public void onTabReselected(Tab tab, android.app.FragmentTransaction ft) {
-      // same tab again- potentially could be a refresh equivalent
-    }
+		// same tab again- potentially could be a refresh equivalent
+		FragmentTransaction sft = mActivity.getFragmentManager().beginTransaction();
+		if (mFragment == null) {
+			// instantiate & initialize fragment for this activity
+			mFragment = Fragment.instantiate(mActivity, mClass.getName(), mfragmentArgs);
+			sft.add(mfragmentContainerId, mFragment, mTag);
+		} else {
+			sft.attach(mFragment);
+		}
+		sft.commit();
+	}
 }
