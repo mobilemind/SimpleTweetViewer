eb537bb39dfa46575aa117e1438d9d6c302d0413
Tom King <mobilemind@pobox.com>
2014-12-08 17:05:37 -0800

    remove unused Key imports from CompoaseActivity.java

--- src/com/yahoo/tomking/simpletweet/ComposeActivity.java
+++ src/com/yahoo/tomking/simpletweet/ComposeActivity.java
@@ -7,9 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnKeyListener;
 import android.widget.EditText;
 import android.widget.Toast;
 
